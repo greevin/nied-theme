2759c0dbd1ab78da8fbd3cef9a686bccad8e009a
Cícero Neto <cicero.alex@gmail.com>
2018-04-17 17:25:20 -0300

    Adiciona 'Imagem Destacada' nas páginas
    Quando as páginas têm imagem destacada, a mesma fica como plano de fundo do jumbotron. Caso não possua, acrescentei um gradiente com alguns tons de azul com as cores do logo.

--- page.php
+++ page.php
@@ -1,3 +1,26 @@
 <?php get_header(); ?>
 
+<!-- verifica se existe texto nessa página -->
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full'); ?>
+  <!-- verifica se existe imagem destacada -->
+  <?php if ( has_post_thumbnail() ) { ?>
+    <div class="jumbotron jumbotron-fluid text-center" style="background-repeat: no-repeat; background-image: url(<?php echo $featured_img_url; ?>); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit; z-index: 20;">
+  <?php } else { ?>
+      <div class="jumbotron jumbotron-fluid text-center">
+  <?php } ?>
+        <div class="container">
+          <h2 class="display-4"><?php the_title(); ?></h2>
+        </div>
+      </div>
+
+<div class="container">
+  <div class="row">
+      <?php the_content(); ?>
+  </div>
+</div>
+<?php
+  endwhile;
+  endif;
+?>
 <?php get_footer(); ?>
