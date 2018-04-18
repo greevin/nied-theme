5b605f87ef976144c89cfbf2624990fd7618598c
Cícero Neto <cicero.alex@gmail.com>
2018-04-18 16:13:32 -0300

    Adicionado rodapé

--- footer.php
+++ footer.php
@@ -1,4 +1,70 @@
 <?php wp_footer(); ?>
 
+<div class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3 col-md-3">
+        <div class="content">
+          <p>LICENÇA</p>
+          <a href="http://creativecommons.org/licenses/by/3.0/deed.pt" rel="license"><img alt="Licença Creative Commons" src="http://i.creativecommons.org/l/by/3.0/88x31.png" style="border-width:0"></a>
+          <p class="my-3">A não ser para conteúdos de terceiros ou quando claramente indicado, os conteúdos disponíveis neste site estão sob uma <a href="http://creativecommons.org/licenses/by/3.0/deed.pt" rel="license">Licença CC-BY</a>.</p>
+        </div>
+      </div>
+      <div class="col-lg-5 col-md-5" style="border-left: 1px solid;border-right: 1px solid;">
+        <div class="content">
+          <p>ENDEREÇO</p>
+          <b>Universidade Estadual de Campinas - UNICAMP</b>
+          <p>Cidade Universitária "Zeferino Vaz"</p>
+          <div class="endereco">
+            <div class="row">
+              <div class="col-md-1" style="margin: auto;">
+                <i class="fa fa-map-pin fa-2x" aria-hidden="true"></i>
+              </div>
+              <div class="col-md-11 info">
+                <small><b>Endereço</b></small>
+                <p>Rua Seis de Agosto, 50, Bloco V da Reitoria - 2º Piso</p>
+                <p>13083-873 | Campinas - SP - Brasil</p>
+              </div>
+            </div>
+            <!-- <i class="fa fa-map-pin fa-2x" aria-hidden="true"></i> -->
+            <!-- <p>Rua Seis de Agosto, 50, Bloco V da Reitoria - 2º Piso</p> -->
+            <!-- <p>13083-873 Campinas - SP - Brasil</p> -->
+          </div>
+          <div class="telefone">
+            <div class="row">
+              <div class="col-md-1" style="margin: auto;">
+                  <i class="fa fa-phone fa-2x" aria-hidden="true"></i>
+              </div>
+              <div class="col-md-11 info">
+                <small><b>Telefone</b></small>
+                <p>+55 19 3521-7350 ou 3521-4353</p>
+                <p>+55 19 3521-7136 (FAX)</p>
+              </div>
+            </div>
+          </div>
+          <div class="fale-conosco">
+            <div class="row">
+              <div class="col-md-1" style="margin: auto;">
+                <i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
+              </div>
+              <div class="col-md-11 info">
+                <small><b>Fale Conosco</b></small>
+                <p class><a href="mailto:contato@nied.unicamp.br">contato@nied.unicamp.br</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4">
+        <p>MAPA</p>
+        <div class="embed-responsive embed-responsive-4by3">
+          <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3677.452971945812!2d-47.068161585035845!3d-22.822724985054663!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8c6ae52803bbb%3A0x4fc2997425647a99!2sR.+Seis+de+Agosto%2C+50+-+Cidade+Universit%C3%A1ria%2C+Campinas+-+SP%2C+13083-873!5e0!3m2!1spt-BR!2sbr!4v1524071193063"></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 </body>
+
 </html>
