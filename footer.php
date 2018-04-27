bcf5e5e939d5d3de5e65a3b1835a9e58b28c0d01
Cícero Neto <cicero.alex@gmail.com>
2018-04-27 17:16:52 -0300

    Divisor para o rodapé

--- footer.php
+++ footer.php
@@ -6,6 +6,7 @@
       <div class="col-lg-3 col-md-3">
         <div class="content">
           <h6>LICENÇA</h6>
+          <div class="divider-footer"></div>
           <a href="http://creativecommons.org/licenses/by/3.0/deed.pt" rel="license"><img alt="Licença Creative Commons" src="http://i.creativecommons.org/l/by/3.0/88x31.png" style="border-width:0"></a>
           <p class="my-3">A não ser para conteúdos de terceiros ou quando claramente indicado, os conteúdos disponíveis neste site estão sob uma <a href="http://creativecommons.org/licenses/by/3.0/deed.pt" rel="license">Licença CC-BY</a>.</p>
         </div>
@@ -13,6 +14,7 @@
       <div class="col-lg-5 col-md-5" style="border-left: 1px solid;border-right: 1px solid;">
         <div class="content">
           <h6>ENDEREÇO</h6>
+          <div class="divider-footer"></div>
           <b>Universidade Estadual de Campinas - UNICAMP</b>
           <p>Cidade Universitária "Zeferino Vaz"</p>
           <div class="endereco">
@@ -26,9 +28,6 @@
                 <p>13083-873 | Campinas - SP - Brasil</p>
               </div>
             </div>
-            <!-- <i class="fa fa-map-pin fa-2x" aria-hidden="true"></i> -->
-            <!-- <p>Rua Seis de Agosto, 50, Bloco V da Reitoria - 2º Piso</p> -->
-            <!-- <p>13083-873 Campinas - SP - Brasil</p> -->
           </div>
           <div class="telefone">
             <div class="row">
@@ -56,6 +55,7 @@
       </div>
       <div class="col-lg-4 col-md-4">
         <h6>MAPA</h6>
+        <div class="divider-footer"></div>
         <div class="embed-responsive embed-responsive-4by3">
           <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3677.452971945812!2d-47.068161585035845!3d-22.822724985054663!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8c6ae52803bbb%3A0x4fc2997425647a99!2sR.+Seis+de+Agosto%2C+50+-+Cidade+Universit%C3%A1ria%2C+Campinas+-+SP%2C+13083-873!5e0!3m2!1spt-BR!2sbr!4v1524071193063"></iframe>
         </div>
