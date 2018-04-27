36457ba24d296bd77d3ebd6e32c9d607f89e98eb
Cícero Neto <cicero.alex@gmail.com>
2018-04-27 11:15:17 -0300

    Pequenas alterações e adicionado link das fontes do Google

--- header.php
+++ header.php
@@ -1,31 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php bloginfo( 'name' ); wp_title('|', true, 'left'); ?></title>
-    <?php wp_head(); ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>
+    <?php bloginfo( 'name' ); wp_title('|', true, 'left'); ?>
+  </title>
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato|Open+Sans|Merriweather" media="screen">
+  <?php wp_head(); ?>
 </head>
+
 <body>
 
-<header>
-  <nav class="navbar navbar-expand-lg navbar-light">
-    <div class="container">
-      <a class="navbar-brand" href="#">
-        <?php
-        if (has_custom_logo()) {
-          the_custom_logo();
-        } else {
-          echo get_bloginfo('name');
-        }
-        ?>
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <!-- caso não tenha o logo, mostra o nome -->
+          <?php
+            if (has_custom_logo()) {
+              the_custom_logo();
+            } else {
+              echo get_bloginfo('name');
+            }
+          ?>
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <?php
+        <!-- menu customizado -->
+        <?php
         wp_nav_menu(array(
             'theme_location'    => 'primary',
             'depth'                => 1, // 1 = with dropdowns, 0 = no dropdowns.
@@ -37,6 +45,6 @@
             'walker'            => new WP_Bootstrap_Navwalker()
         ));
       ?>
-    </div>
-</nav>
-</header>
+      </div>
+    </nav>
+  </header>
