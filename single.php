3393aff01b5662532fd7bcd54f1f15ccdc575801
Cícero Neto <cicero.alex@gmail.com>
2018-04-30 11:44:04 -0300

    Modificado o single.php

--- single.php
+++ single.php
@@ -1,3 +1,19 @@
 <?php get_header(); ?>
+<hr style="margin-top: 0rem;">
+<div class="container" style="margin-top: 30px;margin-bottom: 30px;">
+  <div class="row">
+  	<div class="col-xs-12 col-sm-12">
+  		<?php if (have_posts()): while (have_posts()): the_post(); ?>
+  				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  					<?php the_title('<h2 class="entry-title">', '</h2>'); ?>
+  					<?php if (has_post_thumbnail()): ?>
+  						<div class="pull-right"><?php the_post_thumbnail('thumbnail'); ?></div>
+  					<?php endif; ?>
+  					<?php the_content(); ?>
+  				</article>
+  			<?php endwhile; endif; ?>
+  	</div>
+  </div>
+</div>
 
 <?php get_footer(); ?>
