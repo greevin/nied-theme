46a559c5da5ea27ce9ea3ecc86457c8a90fb6c40
Cícero Neto <cicero.alex@gmail.com>
2018-04-30 17:12:54 -0300

    Página da Biblioteca Digital para a revista

---
+++ page-revista.php
@@ -0,0 +1,34 @@
+<?php
+// Template Name: Pesquisa
+get_header();
+?>
+
+<?php include(TEMPLATEPATH . '/assets/includes/jumbotron.php'); ?>
+
+<!-- noticias -->
+<div class="pesquisa pagina-inicial">
+  <div class="container">
+    <div class="row">
+      <?php
+         $args = array('category_name' => 'revista', 'post_status' => 'publish');
+         $posts = get_posts($args);
+
+         if($posts) : foreach ($posts as $post) : setup_postdata($post);
+      ?>
+      <div class="col-md-3">
+        <div class="card">
+          <a class="card-img-top text-center" href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
+          <div class="card-body">
+            <h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+            <p class="card-text"><a href="<?php the_permalink(); ?>" class="btn btn-link">Leia Mais »</a></p>
+          </div>
+        </div>
+      </div>
+      <?php
+         endforeach;
+         endif;
+      ?>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
