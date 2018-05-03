bf61cbd7121a0816cbb6785f6e98d476767ba3ac
Cícero Neto <cicero.alex@gmail.com>
2018-05-03 09:02:17 -0300

    Ajustado jumbotron das páginas internas para colocar o nome do autor no subtitulo

--- assets/includes/jumbotron.php
+++ assets/includes/jumbotron.php
@@ -1,14 +1,9 @@
 <?php
 // Jumbotron para as páginas internas
 ?>
-<?php $featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>
-<!-- verifica se existe imagem destacada -->
-<?php if (has_post_thumbnail()) { ?>
-<div class="jumbotron jumbotron-fluid text-center" style="background-repeat: no-repeat; background-image: url(<?php echo $featured_img_url; ?>); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit; z-index: 20;">
-  <?php } else { ?>
-  <div class="jumbotron jumbotron-fluid text-center" style="margin-top: 116px;">
-    <?php } ?>
-    <div class="container">
-      <h2 class="display-4 jumbotron-title"><?php the_title(); ?></h2>
-    </div>
+<div class="jumbotron jumbotron-fluid text-center" style="margin-top: 116px;">
+  <div class="container">
+    <h2 class="display-4 jumbotron-title"><?php the_title(); ?></h2>
+    <h5><?php the_field('autor') ?></h5>
   </div>
+</div>
