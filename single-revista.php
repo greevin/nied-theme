d5cb9404f28dfff56f84f13ef2026448778f7df6
Cícero Neto <cicero.alex@gmail.com>
2018-05-07 12:08:28 -0300

    Pequena alteração na distância entre o jumbotron e o começo da página

--- single-revista.php
+++ single-revista.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <hr style="margin-top: 0rem;">
-<div class="container" style="margin-top: 30px;margin-bottom: 30px;">
+<div class="container" style="margin-bottom: 30px;margin-top: 140px;">
   <div class="row">
   	<div class="col-xs-12 col-sm-12">
   		<?php if (have_posts()): while (have_posts()): the_post(); ?>
