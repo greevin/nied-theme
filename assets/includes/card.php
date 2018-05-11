03a1e43302d077a341786c94a7c7fcf87cc6c7c7
Cícero Neto <cicero.alex@gmail.com>
2018-05-10 21:51:40 -0300

    Invertida as cores do Notícias
    Agora o corpo tem uma cor mais escura e as imagens continuam com o fundo branco.

--- assets/includes/card.php
+++ assets/includes/card.php
@@ -5,20 +5,20 @@
   <div class="card mb-4 box-shadow">
     <!-- se o post tiver thumbnail -->
     <?php if( has_post_thumbnail() ): ?>
-      <div class="text-center bg-light card-image-size">
+      <div class="text-center card-image-size">
         <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
           <?php the_post_thumbnail('custom-size'); ?>
         </a>
       </div>
       <!-- senao, apenas mostra o logo -->
     <?php else : ?>
-      <div class="text-center bg-light card-image-size">
+      <div class="text-center card-image-size">
         <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
           <img src="<?php bloginfo('template_url'); ?>/assets/images/logo-transparente.png" alt="<?php the_title_attribute(); ?>">
         </a>
       </div>
     <?php endif; ?>
-    <div class="card-body" style="padding-left: 10px;padding-right: 10px;">
+    <div class="card-body bg-light" style="padding-left: 10px;padding-right: 10px;">
       <h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
       <p class="card-text"><?php the_excerpt(); ?></p>
       <p class="card-text"><a href="<?php the_permalink(); ?>" class="btn btn-link">Leia Mais »</a></p>
