2b2ac6b2168872ab5b9de13b340b9edcf9da82b6
Cícero Neto <cicero.alex@gmail.com>
2018-05-16 09:22:13 -0300

    Pequena alteração no tamanho do jumbotron nas páginas internas

--- assets/includes/jumbotron.php
+++ assets/includes/jumbotron.php
@@ -1,7 +1,7 @@
 <?php
 // Jumbotron para as páginas internas
 ?>
-<div class="jumbotron jumbotron-fluid text-center" style="margin-top: 100px;">
+<div class="pagina-interna jumbotron jumbotron-fluid text-center" style="margin-top: 100px;">
   <div class="container">
     <h2 class="display-4 jumbotron-title"><?php the_title(); ?></h2>
     <h5><?php the_field('autor') ?></h5>
