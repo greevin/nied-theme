bec3340e1dc4e6f19b3f65929b53684d38ef5818
Cícero Neto <cicero.alex@gmail.com>
2018-05-16 09:24:27 -0300

    Arrumada a seção 'Parceiros'

---
+++ assets/includes/parceiro.php
@@ -0,0 +1,8 @@
+<?php
+// Parceiros
+?>
+<div class="col-sm-3 col-md-15" style="height: 80px;display: flex;justify-content: center;align-items: center;">
+  <div class="card text-center">
+    <?php echo get_the_post_thumbnail($post->ID, 'header-thumb-small'); ?>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -74,101 +74,20 @@
 </div>
 
 <!-- parceiros -->
-<div class="parceiros pagina-inicial">
+<section class="parceiros pagina-inicial">
   <div class="container">
-    <div class="page-title" style="margin: 30px 0;text-transform: uppercase;">
-      <h3 class="text-center section-title">Parceiros</h3>
-      <div class="divider"></div>
+    <div class="row text-center">
+      <?php
+         $the_query = new WP_Query( array(
+            'post_type' => 'parceiros',
+            'posts_per_page' => -1,
+         ));
+      ?>
+      <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+        <?php include(TEMPLATEPATH . '/assets/includes/parceiro.php'); ?>
+      <?php endwhile; endif; ?>
     </div>
-    <div class="container-fluid">
-      <!-- primeira linha -->
-      <div class="row">
-        <div class="col">
-          <div class="card mb-4 box-shadow">
-            <a href="http://capes.gov.br/">
-              <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]"
-              src="<?php echo get_template_directory_uri(); ?>/assets/images/logos-parceiros/capes.jpg" data-holder-rendered="true">
-            </a>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card mb-4 box-shadow">
-            <a href="http://www.cnpq.br">
-              <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]"
-              src="<?php echo get_template_directory_uri(); ?>/assets/images/logos-parceiros/cnpq.jpg" data-holder-rendered="true">
-            </a>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card mb-4 box-shadow">
-            <a href="http://www.fapesp.br/">
-              <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]"
-              src="<?php echo get_template_directory_uri(); ?>/assets/images/logos-parceiros/fapesp.png" data-holder-rendered="true">
-            </a>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card mb-4 box-shadow">
-            <a href="http://www.fundap.sp.gov.br/">
-              <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]"
-              src="<?php echo get_template_directory_uri(); ?>/assets/images/logos-parceiros/fundap.png" data-holder-rendered="true">
-            </a>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card mb-4 box-shadow">
-            <a href="http://www.itaucultural.org.br/">
-              <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]"
-              src="<?php echo get_template_directory_uri(); ?>/assets/images/logos-parceiros/itau_cultural.png" data-holder-rendered="true">
-            </a>
-          </div>
-        </div>
-    </div>
-    <!-- segunda linha -->
-    <div class="row">
-      <div class="col">
-        <div class="card mb-4 box-shadow">
-          <a href="http://www.nied.unicamp.br/oea/">
-            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]"
-            src="<?php echo get_template_directory_uri(); ?>/assets/images/logos-parceiros/oea.gif" data-holder-rendered="true">
-          </a>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card mb-4 box-shadow">
-          <a href="http://www.preac.unicamp.br/">
-            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]"
-            src="<?php echo get_template_directory_uri(); ?>/assets/images/logos-parceiros/preac.png" data-holder-rendered="true">
-          </a>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card mb-4 box-shadow">
-          <a href="http://www.prg.unicamp.br/portal/">
-            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]"
-            src="<?php echo get_template_directory_uri(); ?>/assets/images/logos-parceiros/prg.png" data-holder-rendered="true">
-          </a>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card mb-4 box-shadow">
-          <a href="http://www.hewlett.org/">
-            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]"
-            src="<?php echo get_template_directory_uri(); ?>/assets/images/logos-parceiros/the-william-and-flora-hewlett-foundation.png" data-holder-rendered="true">
-          </a>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card mb-4 box-shadow">
-          <a href="http://www.unesco.org/new/pt/brasilia/">
-            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]"
-            src="<?php echo get_template_directory_uri(); ?>/assets/images/logos-parceiros/unesco.jpg" data-holder-rendered="true">
-          </a>
-        </div>
-      </div>
-    </div>
-</div>
   </div>
-</div>
+</section>
 
 <?php get_footer(); ?>
