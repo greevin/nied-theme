569c8b3fcbf87f8287788c19bee92d52b2ec6afb
Cícero Neto <cicero.alex@gmail.com>
2018-05-17 15:07:37 -0300

    Adicionada seção 'Revista' com ACF

--- index.php
+++ index.php
@@ -33,34 +33,42 @@
 </section>
 
 <!-- revista -->
-<div class="revista pagina-inicial" style="background:#BBD5E7">
+<section class="revista pagina-inicial" style="background:#BBD5E7">
   <?php
-     $args = array('post_type' => 'page', 'pagename' => 'revistas');
-     $revista = get_posts($args);
-
-     if($revista) : foreach ($revista as $post) : setup_postdata($post);
-  ?>
+  $args = array(
+    'name'      => 'revistas',
+    'post_type' => 'configuracoes_tema',
+    'post_status' => 'publish'
+  );
+  $edicao_atual_loop = new WP_Query( $args );
+  if ( $edicao_atual_loop->have_posts() ) :
+    while ( $edicao_atual_loop->have_posts() ) : $edicao_atual_loop->the_post();
+      $texto_apresentacao = get_field('texto_apresentacao');
+      $imagem_da_edicao_atual = get_field('imagem_da_edicao_atual');
+      $link_edicao_atual = get_field('link_edicao_atual');
+?>
   <div class="container">
-    <div class="page-title" style="margin: 30px 0;text-transform: uppercase;">
-      <h3 class="text-center section-title"><?php the_title(); ?></h3>
+    <div class="section-title">
+      <h3>Revista</h3>
       <div class="divider"></div>
-    </div>
-    <div class="row">
-      <div class="col-md-4 col-lg-4 text-center">
-        <?php the_post_thumbnail(false, array('class' => 'img-fluid')) ?>
+      <div class="row">
+        <div class="col-md-4 col-lg-4 text-center">
+          <img alt="Licença Creative Commons" src="<?php echo $imagem_da_edicao_atual; ?>" style="border-width:0">
+        </div>
+        <div class="col-md-8 col-lg-8">
+          <?php echo $texto_apresentacao; ?>
+          <a href="<?php echo $link_edicao_atual; ?>" class="btn btn-primary">Edição Atual</a>
+          <a href="http://nied.test/revista/" class="btn btn-secondary">Edições Anteriores</a>
+        </div>
       </div>
-      <div class="col-md-8 col-lg-8">
-        <?php the_content(); ?>
-        <a href="http://nied.test/v-4-n-1-2017/" class="btn btn-primary">Edição Atual</a>
-        <a href="http://nied.test/revista/" class="btn btn-secondary">Edições Anteriores</a>
-      </div>
     </div>
+    <?php
+  endwhile;
+    wp_reset_postdata();
+  endif;
+    ?>
   </div>
-  <?php
-     endforeach;
-     endif;
-  ?>
-</div>
+</section>
 
 <!-- linha do tempo -->
 <div class="linha pagina-inicial" style="background: #f5f6f8;">
