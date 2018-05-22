80af19d4251635d87faa6d185944f55a8364ab64
Cícero Neto <cicero.alex@gmail.com>
2018-05-22 08:31:49 -0300

    Alterado a página 'Pesquisa' para usar o card personalizado

--- page-pesquisa.php
+++ page-pesquisa.php
@@ -15,21 +15,8 @@ get_header();
 
          if($posts) : foreach ($posts as $post) : setup_postdata($post);
       ?>
-      <div class="col-md-4">
-        <div class="card">
-          <?php $urlImg = wp_get_attachment_url(get_post_thumbnail_id(get_the_ID())); ?>
-          <a class="card-img-top text-center" href="<?php the_permalink(); ?>">
-            <div class="<?php echo $urlImg == false ? 'fundo-logo' : 'fundo-branco'; ?>">
-    				      <div class="blog-element" style="background-image: url(<?php echo $urlImg; ?>);"></div>
-    			  </div>
-          </a>
-          <div class="card-body">
-            <h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-            <p class="card-text"><?php the_excerpt(); ?></p>
-            <p class="card-text"><a href="<?php the_permalink(); ?>" class="btn btn-link">Leia Mais »</a></p>
-          </div>
-        </div>
-      </div>
+      <?php include(TEMPLATEPATH . '/assets/includes/card.php'); ?>
+
       <?php
          endforeach;
          endif;
