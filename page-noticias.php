bbea1c99e4bab0d8adbcbb18ba4dee549229ca45
Cícero Neto <cicero.alex@gmail.com>
2018-05-24 11:15:40 -0300

    Alterado o tamanho do logo da página inicial

---
+++ page-noticias.php
@@ -0,0 +1,27 @@
+<?php
+// Template Name: Pesquisa
+get_header();
+?>
+
+<?php include(TEMPLATEPATH . '/assets/includes/jumbotron.php'); ?>
+
+<!-- noticias -->
+<div class="pesquisa pagina-inicial">
+  <div class="container">
+    <div class="row">
+      <?php
+         $args = array('category_name' => 'noticias, eventos', 'post_status' => 'publish');
+         $posts = get_posts($args);
+
+         if($posts) : foreach ($posts as $post) : setup_postdata($post);
+      ?>
+      <?php include(TEMPLATEPATH . '/assets/includes/card.php'); ?>
+
+      <?php
+         endforeach;
+         endif;
+      ?>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
