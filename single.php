6836183a50793b2932b98cba8bed4478957964c3
Cícero Neto <cicero.alex@gmail.com>
2018-05-28 08:15:24 -0300

    Alterado single.php

--- single.php
+++ single.php
@@ -1,10 +1,19 @@
-<?php
-$post = $wp_query->post;
+<?php get_header(); ?>
 
-if ( in_category( 'revista' ) ) {
-  include( TEMPLATEPATH.'/single-revista.php' );
-}
-else {
-  include( TEMPLATEPATH.'/single-generic.php' );
-}
-?>
+<div class="container" style="margin-bottom: 30px;margin-top: 140px;">
+  <div class="row">
+  	<div class="col-xs-12 col-sm-12">
+  		<?php if (have_posts()): while (have_posts()): the_post(); ?>
+  				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  					<?php the_title('<h2 class="entry-title">', '</h2>'); ?>
+  					<?php if (has_post_thumbnail()): ?>
+  						<div class="pull-right"><?php the_post_thumbnail('thumbnail'); ?></div>
+  					<?php endif; ?>
+  					<?php the_content(); ?>
+  				</article>
+  			<?php endwhile; endif; ?>
+  	</div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
