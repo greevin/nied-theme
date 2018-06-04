06fc15ca948a0f867b8bd5cb20cc543bd1d33250
Cícero Neto <cicero.alex@gmail.com>
2018-06-04 11:09:50 -0300

    Adicionado para mostrar as informações do Evento
    Quando a tag selecionada é Eventos, ele mostra as informações do horário e da data. Quando a tag for Noticias, ele mostra o resumo.

--- assets/includes/card.php
+++ assets/includes/card.php
@@ -22,10 +22,46 @@
       <div class="card-category">
         <?php the_category( ' | ' ); ?>
       </div>
+    <?php
+      $categories = get_the_category();
+      if($categories[0]->name !== 'Evento') :
+    ?>
       <h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
       <p class="card-text"><?php the_excerpt(); ?></p>
       <p class="card-text"><a href="<?php the_permalink(); ?>" class="btn btn-link">Leia Mais »</a></p>
       <div class="d-flex justify-content-between align-items-center"></div>
+    <?php else : ?>
+       <h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+       <?php if( get_field('data_de_inicio') ): ?>
+           <?php
+             $start_date = get_field('data_de_inicio', false, false);
+             $start_date = new DateTime($start_date);
+
+             $end_date = get_field('data_de_fim', false, false);
+             $end_date = new DateTime($end_date);
+           ?>
+           <span class="card-text">Data: </span>
+           <span><?php echo $start_date->format('d/m/Y'); ?></span>
+           <?php if( get_field('data_de_fim') &&  $start_date->format('d/m/Y') != $end_date->format('d/m/Y')): ?>
+             <span> - <?php echo $end_date->format('d/m/Y'); ?></span>
+           <?php endif; ?>
+           <br>
+       <?php endif; ?>
+       <?php if( get_field('horario_de_inicio') ): ?>
+           <span>Horário: </span>
+           <span><?php the_field('horario_de_inicio'); ?></span>
+           <?php if( get_field('horario_de_fim') ): ?>
+             <span> - <?php the_field('horario_de_fim'); ?></span>
+           <?php endif; ?>
+          <br>
+       <?php endif; ?>
+       <?php if( get_field('local') ): ?>
+         <span>Local: </span>
+         <span><?php the_field('local'); ?></span>
+       <?php endif; ?>
+           <p class="card-text"><a href="<?php the_permalink(); ?>" class="btn btn-link">Leia Mais »</a></p>
+           <div class="d-flex justify-content-between align-items-center"></div>
+      <?php endif; ?>
     </div>
   </div>
 </div>
