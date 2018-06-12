18724d56d4331862a5eb744fdc121068598a882a
Cícero Neto <cicero.alex@gmail.com>
2018-06-12 20:17:44 -0300

    Corrigido logo dos parceiros

--- assets/includes/parceiro.php
+++ assets/includes/parceiro.php
@@ -2,8 +2,8 @@
 // Parceiros
 ?>
 
-<div class="col-sm-3 col-md-15" style="display: flex;justify-content: center;align-items: center;">
-  <div class="card text-center">
-    <a href="<?php the_field('site_parceiro'); ?>"><?php echo get_the_post_thumbnail($post->ID, 'header-thumb-small'); ?></a>
+<div class="col-sm-3 col-md-15 parceiros">
+  <div class="card text-center" style="margin: 0 auto;">
+    <a href="<?php the_field('site_parceiro'); ?>"><?php echo get_the_post_thumbnail($post->ID); ?></a>
   </div>
 </div>
