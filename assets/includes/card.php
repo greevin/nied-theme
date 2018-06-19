13360ded58029dbb3eac2786fb014c046550997e
Cícero Neto <cicero.alex@gmail.com>
2018-06-19 20:43:28 -0300

    Adicionado um espaçamento nas informações do evento no card

--- assets/includes/card.php
+++ assets/includes/card.php
@@ -32,36 +32,38 @@
       <div class="d-flex justify-content-between align-items-center"></div>
     <?php else : ?>
        <h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-       <?php if( get_field('data_de_inicio') ): ?>
-           <?php
-             $start_date = get_field('data_de_inicio', false, false);
-             $start_date = new DateTime($start_date);
+       <p class="card-text">
+         <?php if( get_field('data_de_inicio') ): ?>
+             <?php
+               $start_date = get_field('data_de_inicio', false, false);
+               $start_date = new DateTime($start_date);
 
-             $end_date = get_field('data_de_fim', false, false);
-             $end_date = new DateTime($end_date);
-           ?>
-           <span class="card-text">Data: </span>
-           <span><?php echo $start_date->format('d/m/Y'); ?></span>
-           <?php if( get_field('data_de_fim') &&  $start_date->format('d/m/Y') != $end_date->format('d/m/Y')): ?>
-             <span> - <?php echo $end_date->format('d/m/Y'); ?></span>
-           <?php endif; ?>
-           <br>
-       <?php endif; ?>
-       <?php if( get_field('horario_de_inicio') ): ?>
-           <span>Horário: </span>
-           <span><?php the_field('horario_de_inicio'); ?></span>
-           <?php if( get_field('horario_de_fim') ): ?>
-             <span> - <?php the_field('horario_de_fim'); ?></span>
-           <?php endif; ?>
-          <br>
-       <?php endif; ?>
-       <?php if( get_field('local') ): ?>
-         <span>Local: </span>
-         <span><?php the_field('local'); ?></span>
-       <?php endif; ?>
-           <p class="card-text"><a href="<?php the_permalink(); ?>" class="btn btn-link">Leia Mais »</a></p>
-           <div class="d-flex justify-content-between align-items-center"></div>
-      <?php endif; ?>
+               $end_date = get_field('data_de_fim', false, false);
+               $end_date = new DateTime($end_date);
+             ?>
+             <span class="card-text">Data: </span>
+             <span><?php echo $start_date->format('d/m/Y'); ?></span>
+             <?php if( get_field('data_de_fim') &&  $start_date->format('d/m/Y') != $end_date->format('d/m/Y')): ?>
+               <span> - <?php echo $end_date->format('d/m/Y'); ?></span>
+             <?php endif; ?>
+             <br>
+         <?php endif; ?>
+         <?php if( get_field('horario_de_inicio') ): ?>
+             <span>Horário: </span>
+             <span><?php the_field('horario_de_inicio'); ?></span>
+             <?php if( get_field('horario_de_fim') ): ?>
+               <span> - <?php the_field('horario_de_fim'); ?></span>
+             <?php endif; ?>
+            <br>
+         <?php endif; ?>
+         <?php if( get_field('local') ): ?>
+           <span>Local: </span>
+           <span><?php the_field('local'); ?></span>
+         <?php endif; ?>
+             <p class="card-text"><a href="<?php the_permalink(); ?>" class="btn btn-link">Leia Mais »</a></p>
+             <div class="d-flex justify-content-between align-items-center"></div>
+        <?php endif; ?>
+       </p>
     </div>
   </div>
 </div>
