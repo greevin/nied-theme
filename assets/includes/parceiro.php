fd9cb54ab6ebd8ddb331bb79671c85c484e57f88
Cícero Neto <cicero.alex@gmail.com>
2018-06-20 16:53:16 -0300

    Ajustado tamanho dos logos dos parceiros

--- assets/includes/parceiro.php
+++ assets/includes/parceiro.php
@@ -3,7 +3,7 @@
 ?>
 
 <div class="col-sm-3 col-md-15 parceiros">
-  <div class="card text-center" style="margin: 0 auto;">
+  <div style="max-width: 110px;margin: 2px;">
     <a href="<?php the_field('site_parceiro'); ?>"><?php echo get_the_post_thumbnail($post->ID); ?></a>
   </div>
 </div>
