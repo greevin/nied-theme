c520325a81a0efb7bd768dcda7c783c51db477f3
Cícero Neto <cicero.alex@gmail.com>
2018-06-20 19:34:58 -0300

    Retirado o sombreamento dos cards na Equipe

--- assets/includes/card-equipe.php
+++ assets/includes/card-equipe.php
@@ -2,7 +2,7 @@
 // Card utilizado na página 'Equipe'
 ?>
 <div class="col-md-4 col-lg-3">
-  <div class="card text-center">
+  <div class="text-center">
     <div class="card-image">
       <?php $urlImg = wp_get_attachment_url(get_post_thumbnail_id(get_the_ID())); ?>
       <div class="circle-image fundo-azul">
