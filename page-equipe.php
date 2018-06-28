edf30e880cf71bd3fada4976d2e81659b55a34fd
Cícero Neto <cicero.alex@gmail.com>
2018-06-28 16:25:26 -0300

    Corrigido para mostrar o divisor das seções nas páginas

--- page-equipe.php
+++ page-equipe.php
@@ -8,9 +8,9 @@ get_header();
 <!-- equipes -->
 <div class="equipe pagina-inicial" style="background: white;">
   <div class="container">
-    <div class="equipe-atual">
-      <div class="divider"></div>
-      <h3 class="text-center section-title" style="margin-bottom: 30px;">Atual</h3>
+    <div class="equipe-atual section-title">
+      <h3 class="text-center">Atual</h3>
+      <div class="divider" style="width: 100px;"></div>
       <div class="row">
         <?php
            $args = array('post_type' => 'equipe', 'post_status' => 'publish', 'posts_per_page' => -1, 'orderby'=> 'title', 'order' => 'ASC');
@@ -28,9 +28,9 @@ get_header();
       </div>
     </div>
 
-    <div class="equipe-anterior">
-      <div class="divider"></div>
-      <h3 class="text-center section-title" style="margin-bottom: 30px;">Anterior</h3>
+    <div class="equipe-anterior section-title">
+      <h3 class="text-center">Anterior</h3>
+      <div class="divider" style="width: 100px;"></div>
       <div class="row">
         <?php
            $args = array('post_type' => 'equipe', 'post_status' => 'publish', 'posts_per_page' => -1, 'orderby'=> 'title', 'order' => 'ASC');
--- page-pesquisa.php
+++ page-pesquisa.php
@@ -8,9 +8,9 @@ get_header();
 <!-- Projetos -->
 <div class="pesquisa pagina-inicial" style="background: white;">
   <div class="container">
-    <div class="em-andamento">
-      <div class="divider"></div>
-      <h3 class="text-center section-title" style="margin-bottom: 30px;">Em Andamento</h3>
+    <div class="em-andamento section-title">
+      <h3 class="text-center">Em Andamento</h3>
+      <div class="divider" style="width: 100px;"></div>
       <div class="row grid-row">
         <?php
            $args = array('post_type' => 'projeto', 'post_status' => 'publish', 'posts_per_page' => -1, 'meta_key' => 'data_de_inicio', 'orderby'=> 'meta_value', 'order' => 'DESC');
@@ -28,9 +28,9 @@ get_header();
       </div>
     </div>
 
-    <div class="finalizados">
-      <div class="divider"></div>
-      <h3 class="text-center section-title" style="margin-bottom: 30px;">Finalizados</h3>
+    <div class="finalizados section-title">
+      <h3 class="text-center">Finalizados</h3>
+      <div class="divider" style="width: 100px;"></div>
       <div class="row grid-row">
         <?php
            $args = array('post_type' => 'projeto', 'post_status' => 'publish', 'posts_per_page' => -1, 'meta_key' => 'data_de_inicio', 'orderby'=> 'meta_value', 'order' => 'DESC');
