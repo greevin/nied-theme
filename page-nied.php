bbcf834eedaf75fb86fcff2f907808e10cbab49c
Cícero Neto <cicero.alex@gmail.com>
2018-07-03 07:24:02 -0300

    Adicionado divisor do título na página NIED

--- page-nied.php
+++ page-nied.php
@@ -16,19 +16,20 @@ get_header();
       <div class="row" style="margin: 20px 0;">
         <div class="col-lg-4">
           <h5>Missão</h5>
+          <div class="divider" style="width: 100px;border-bottom: 3px solid;color: #007AB8;margin-bottom: 20px;"></div>
     			<?php the_field('nied_missao', $nied); ?>
         </div>
         <div class="col-lg-4" style="border-left: 1px solid;border-right: 1px solid;">
           <h5>Objetivo</h5>
+          <div class="divider" style="width: 100px;border-bottom: 3px solid;color: #007AB8;margin-bottom: 20px;"></div>
     			<?php the_field('nied_objetivo'); ?>
         </div>
         <div class="col-lg-4">
           <h5>Visão</h5>
+          <div class="divider" style="width: 100px;border-bottom: 3px solid;color: #007AB8;margin-bottom: 20px;"></div>
     			<?php the_field('nied_visao'); ?>
         </div>
       </div>
-
-      <?php the_field('album_de_fotos'); ?>
   </div>
 </div>
 <?php
